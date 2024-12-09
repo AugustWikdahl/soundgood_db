3751ce25c976dbca0027c7ce47db77265325ee93
Filip Boive <56317185+bangobongo@users.noreply.github.com>
2024-12-09 13:19:12 +0100

    changes

--- src/main/startup/Main.java
+++ src/main/startup/Main.java
@@ -4,4 +4,8 @@ import main.integration.SoundgoodDAO;
 
 public class Main {
     SoundgoodDAO soundgoodDAO = new SoundgoodDAO();
+    public static void main(String[] args) {
+        System.out.println("Program started!");
+        Main main = new Main();
+    }
 }
