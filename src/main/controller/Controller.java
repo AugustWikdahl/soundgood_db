1f6d4a6696fcbbba105a8c7f1d73edba30252b2a
Casper Johansson <caspthepro@gmail.com>
2024-12-11 11:34:19 +0100

    added skeleton code for controller

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,19 +1,35 @@
 package main.controller;
 
 import main.integration.SoundgoodDAO;
+
+import java.util.List;
+
 import main.integration.DBException;
+import main.model.Instrument;
+import main.model.InstrumentDTO;
 
+public class Controller {
+    private final SoundgoodDAO soundgoodDb;
 
- public class Controller {
-     private final SoundgoodDAO soundgoodDb;
- 
-     /**
-      * Creates a new instance, and retrieves a connection to the database.
-      * 
-      * @throws BankDBException If unable to connect to the database.
-      */
-     public Controller() throws DBException {
-         
-     } 
- 
- }
+    /**
+     * Creates a new instance, and retrieves a connection to the database.
+     * 
+     * @throws BankDBException If unable to connect to the database.
+     */
+    public Controller() throws DBException {
+        this.soundgoodDb = new SoundgoodDAO();
+    }
+
+    public List<Instrument> getAvailableInstrumentsOfType (String instrumentType) {
+        try {
+            soundgoodDb.findAvailableInstruments(instrumentType);
+        } catch (DBException e) {
+            System.out.println("Fuck yourself");
+        }
+    }
+
+    public void rentInstrument (String studentId, String instrumentId) {
+
+    }
+
+}
